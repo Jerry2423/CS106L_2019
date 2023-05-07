a2a412066f467d824a8a001c2acf0e8337d2e141
Jerry Xu <xjr2423@gmail.com>
2023-05-07 18:12:58 +0800

    MyVector1.0

--- CS106L_example_code/VectorClass - Move/MyVector.h
+++ CS106L_example_code/VectorClass - Move/MyVector.h
@@ -1,194 +1,275 @@
-/*
- * Move Semantics Lecture Code
- *
- * Note: for simplicity, we've removed the iterator class, and instead
- * used a simple pointer. For the GapBuffer, the iterator class needs to be implemented!
- */
+#ifndef MYVECTOR_H //order matters: isndef first then define
+#define MYVECTOR_H
 
-#include <string>
+// #include "feaction.h"
 #include <algorithm>
+#include <cstddef>
 #include <iostream>
+#include <iterator>
+#include <ostream>
+#include <string>
+#include <initializer_list>
+using namespace std;
 
-using std::endl;
-using std::cout;
+/*
+assumptions:
+datatype: value_type
+referece(const): value_type&
+sizetype: size_type
+iterator/pointer: value_type*
+   */
 
-template <typename T>
+
+template<typename T>
 class MyVector {
 public:
-    using value_type = T;
-    using iterator = value_type*;
-    using const_iterator = value_type*;
-    using size_type = size_t;
 
-    const size_type kInitialSize = 10;
-    MyVector() noexcept;
-    MyVector(size_type n, const value_type& val) noexcept;
-    MyVector(const MyVector<T>& other) noexcept;
-    MyVector<T>& operator=(const MyVector<T>& rhs) noexcept;
-    // MyVector(MyVector<T>&& other) noexcept;
-    // MyVector<T>& operator=(MyVector<T>&& rhs) noexcept;
-    ~MyVector() noexcept;
+    using value_type = T;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using size_type = size_t;
+    using iterator = value_type*;
+
+
+    MyVector();
+    MyVector(size_type n, const_reference val);
+    // MyVector(size_type n);
+    MyVector(std::initializer_list<value_type> l);
+
+    //addition: rule of three: copy constructor, copy assignment, destructor
+    ~MyVector();
+    MyVector(const MyVector<T>& other);
+    MyVector<T>& operator=(const MyVector<T>& rhs); //assignment
+    MyVector(MyVector<T>&& other);
+    MyVector<T>& operator=(MyVector<T>&& rhs);
 
     size_type size() const;
     bool empty() const;
+    void reserve(size_type new_cap);
 
-    value_type& operator[](size_type indx);
-    const value_type& operator[](size_type indx) const;
-    value_type& at(size_type indx);
+    //add l-value version for insert, push_back and +=
+    
+    void insert(size_type index, const_reference val); //!
+    void insert(size_type index, value_type&& val);
+    
+    void push_back(value_type&& value);
+    void push_back(const_reference value); //!
+    
+    MyVector& operator+=(const_reference val);
+    MyVector& operator+=(value_type&& val); //!
+    
+    const_reference at(size_type n) const;
+    reference at(size_type n);
 
-    iterator insert(iterator pos, const value_type& elem);
-    void push_back(const value_type& elem);
+    reference operator[](size_type n);
+    const_reference operator[](size_type n) const;
+    
 
-    iterator begin();
-    iterator end();
-    const_iterator begin() const;
-    const_iterator end() const;
 private:
-    void grow();
-    value_type* elems;
-    size_type logicalSize;
-    size_type allocatedSize;
+    size_type _logicalSize;
+    size_type _allocateSize;
+    iterator _elems;
+
+    friend ostream& operator<<(ostream& os, const MyVector<T>& v);
+    friend MyVector<T> operator+(const MyVector<T>& lhs, const MyVector<T>& rhs);
 };
 
-
-template <typename T>
-MyVector<T>::MyVector() noexcept :
-    logicalSize(0), allocatedSize(kInitialSize)
-{
-    std::cout << "Hello from the default constructor!" << std::endl;
-    elems = new value_type[allocatedSize];
+template<typename T>
+MyVector<T>::MyVector():_logicalSize(0), _allocateSize(10), _elems(new value_type[_allocateSize]) {
+    // cout << "defalut constructor" << endl;
 }
 
-template <typename T>
-MyVector<T>::MyVector(size_type n, const value_type &val) noexcept :
-    logicalSize(n), allocatedSize(2*n)
-{
-    std::cout << "Hello from the fill constructor!" << std::endl;
-    elems = new value_type[allocatedSize];
-    std::fill(begin(), end(), val);
+template<typename T>
+MyVector<T>::MyVector(size_type n, const_reference val): _logicalSize(n), _allocateSize(2*n) {
+    // cout << "fill constructor" << endl;
+    _elems = new value_type[_allocateSize];
+    std::fill(_elems, _elems+_logicalSize, val);
+} //syntax: new type[size]{filled in value}; add one more {}
+
+// DoubleVector::DoubleVector(size_t n) :_logicalSize(n), _allocateSize(2*n), _elems(new double[_allocateSize]{0}) {}
+template<typename T>
+MyVector<T>::~MyVector() {
+    // cout << "destructor" << endl;
+    delete [] _elems;
 }
 
-template <typename T>
-MyVector<T>::MyVector(const MyVector<T>& other) noexcept :
-    logicalSize(other.logicalSize), allocatedSize(other.allocatedSize) {
-    std::cout << "Hello from the copy constructor!" << std::endl;
-    elems = new value_type[allocatedSize];
-    std::copy(other.begin(), other.end(), begin());
+template<typename T>
+MyVector<T>::MyVector(std::initializer_list<value_type> l): _logicalSize(l.size()), _allocateSize(2*l.size()), _elems(new value_type[_allocateSize]) {
+    // cout << "initializer_list constructor" << endl;
+    std::copy(l.begin(), l.end(), _elems);
 }
 
-template <typename T>
-MyVector<T>& MyVector<T>::operator=(const MyVector<T>& rhs) noexcept {
-    std::cout << "Hello from the copy assignment operator!" << std::endl;
-    if (this != &rhs) {
-        delete [] elems;
-        logicalSize = rhs.logicalSize;
-        allocatedSize = rhs.allocatedSize;
-        elems = new value_type[allocatedSize];
-        std::copy(rhs.begin(), rhs.end(), begin());
+template<typename T>
+MyVector<T>::MyVector(const MyVector<T>& other):_logicalSize(other._logicalSize), _allocateSize(other._allocateSize), _elems(new value_type[_allocateSize]) {
+    // cout << "copy constructor" << endl;
+    std::copy(other._elems, other._elems+other.size(), _elems);
+}
+
+template<typename T>
+MyVector<T>& MyVector<T>::operator=(const MyVector<T>& rhs) {
+    cout << "coppy assignment" << endl;
+    if (this != &rhs){
+        _logicalSize = rhs._logicalSize;
+        _allocateSize = rhs._allocateSize;
+        delete [] _elems;
+        _elems = new value_type[_allocateSize];
+        std::copy(rhs._elems, rhs._elems + rhs._allocateSize, _elems);
     }
     return *this;
 }
 
-// template <typename T>
-// MyVector<T>::MyVector(MyVector<T>&& other) noexcept :
-//     elems(std::move(other.elems)),
-//     logicalSize(std::move(other.logicalSize)),
-//     allocatedSize(std::move(other.allocatedSize)) {
-//
-//     std::cout << "Hello from the move constructor!" << std::endl;
-//     other.elems = nullptr;
-// }
-
-// template <typename T>
-// MyVector<T>& MyVector<T>::operator=(MyVector<T>&& rhs) noexcept {
-//     std::cout << "Hello from the move assignment operator!" << std::endl;
-//     if (this != &rhs) {
-//         delete [] elems;
-//         logicalSize = std::move(rhs.logicalSize);
-//         allocatedSize = std::move(rhs.allocatedSize);
-//         elems = std::move(rhs.elems);
-//         rhs.elems = nullptr;
-//     }
-//     return *this;
-// }
-
-template <typename T>
-MyVector<T>::~MyVector<T>() noexcept {
-    std::cout << "Hello from the destructor" << std::endl;
-    delete[] elems;
+template<typename T>
+MyVector<T>::MyVector(MyVector<T>&& other): _logicalSize(other.size()), _allocateSize(other._allocateSize), _elems(other._elems) {
+    // cout << "move constructor" << endl;
+    other._elems = nullptr;
+    other._allocateSize = 0;
+    other._logicalSize = 0;
 }
 
-template <typename T>
-typename MyVector<T>::iterator MyVector<T>::begin() {
-    return elems;
+template<typename T>
+MyVector<T>& MyVector<T>::operator=(MyVector<T>&& rhs) {
+    cout << "move assignment" << endl;
+    if (this != &rhs) {
+        delete [] _elems;
+        _logicalSize = std::move(rhs._logicalSize);
+        _allocateSize = std::move(rhs._allocateSize);
+        _elems = rhs._elems;
+        rhs._elems = nullptr;
+    }
+    return *this;
 }
 
-template <typename T>
-typename MyVector<T>::iterator MyVector<T>::end() {
-    return elems + size();
+//size_type is dependent name, in order to use dependent name, we need tp gp to the typename(declare using dependent name) MyVector<T>::size_type
+template<typename T>
+ typename MyVector<T>::size_type MyVector<T>:: size() const {
+    return _logicalSize;
 }
 
-template <typename T>
-typename MyVector<T>::const_iterator MyVector<T>::begin() const {
-    return elems;
-}
-
-template <typename T>
-typename MyVector<T>::const_iterator MyVector<T>::end() const {
-    return elems + size();
-}
-
-template <typename T>
-typename MyVector<T>::size_type MyVector<T>::size() const {
-    return logicalSize;
-}
-
-template <typename T>
+template<typename T>
 bool MyVector<T>::empty() const {
-    return size() == 0;
+    return _logicalSize == 0;
 }
 
-template <typename T>
-typename MyVector<T>::value_type& MyVector<T>::operator[](size_type index) {
-    return *(begin() + index);
-}
-
-template <typename T>
-typename MyVector<T>::value_type& MyVector<T>::at(size_type index) {
-    if(index >= size()) {
-        throw std::out_of_range("Out of bounds access of vector");
+template<typename T>
+void MyVector<T>::reserve(size_type new_cap) {
+    if (new_cap > _allocateSize) {
+        iterator temp = new value_type[new_cap];
+        std::move(_elems, _elems+_allocateSize, temp);
+        delete [] _elems; //??
+        _elems = temp;
+        _allocateSize = new_cap;
     }
-    return *(begin() + index);
 }
 
-template <typename T>
-void MyVector<T>::grow() {
-    iterator new_elems = new value_type[2*allocatedSize];
-    std::copy(begin(), end(), new_elems);
-    delete[] elems;
-    allocatedSize *= 2;
-    elems = new_elems;
-}
-
-template <typename T>
-typename MyVector<T>::iterator MyVector<T>::insert(iterator pos, const value_type &elem) {
-    size_type indx = pos - begin();
-    if(size() == allocatedSize) {
-        grow();
+template<typename T>
+void MyVector<T>::insert(size_type index, const_reference val) {
+    if (_allocateSize == _logicalSize) {
+        reserve(_allocateSize * 2);
     }
-    /*
-      * If we grew, we need pos to point to the new array
-      * since the previous address of pos would be invalid
-      */
-    pos = begin() + indx;
-    std::copy_backward(pos, end(), end() + 1);
-    *pos = elem;
-    ++logicalSize;
-    return pos;
+    std::move_backward(_elems+index, _elems+_logicalSize, _elems+_logicalSize+1);
+    _elems[index] = val;
+    _logicalSize += 1;
 }
 
-template <typename T>
-void MyVector<T>::push_back(const value_type &elem) {
-    insert(end(), elem);
+template<typename T>
+void MyVector<T>::insert(size_type index, value_type&& val) {
+    if (_allocateSize == _logicalSize) {
+        reserve(2*_allocateSize);
+    }
+    std::move_backward(_elems+index, _elems+_logicalSize, _elems+_logicalSize+1); //?
+    _elems[index] = std::move(val);
+    _logicalSize += 1;
 }
+
+template<typename T>
+void MyVector<T>::push_back(value_type&& value) {
+    insert(_logicalSize, std::move(value));
+}
+
+template<typename T>
+void MyVector<T>::push_back(const_reference val) {
+    insert(_logicalSize, val);
+}
+
+template<typename T>
+typename MyVector<T>::reference MyVector<T>::at(size_type n) {
+    if (n >= _logicalSize) {
+        throw "invalid index";
+    } else {
+        return _elems[n];
+    }
+}
+
+template<typename T>
+typename MyVector<T>::const_reference MyVector<T>::at(size_type n) const {
+    if (n >= _logicalSize) {
+        throw "invalid index";
+    } else  {
+        return _elems[n];
+    }
+}
+
+template<typename T>
+typename MyVector<T>::reference MyVector<T>::operator[](size_type n) {
+
+    if (n >= _logicalSize) {
+        throw "invalid index";
+    } else {
+        return _elems[n];
+    }
+}
+
+template<typename T>
+typename MyVector<T>::const_reference MyVector<T>::operator[](size_type n) const {
+    
+    if (n >= _logicalSize) {
+        throw "invalid index";
+    } else {
+        return _elems[n];
+    }
+}
+
+template<typename T>
+ostream& operator<<(ostream& os, const MyVector<T>& v) {
+    os << "{" << v._elems[0];
+    for (auto i = 1; i < v._logicalSize; i++) {
+        os << ", " << v._elems[i];
+    }
+    os << "}";
+    return os;
+}
+
+template<typename T>
+MyVector<T> operator+(const MyVector<T>& lhs, const MyVector<T>& rhs) {
+    MyVector<T> v = lhs;
+    // std::move(lhs._elems, lhs._elems + lhs.size(), v._elems);
+    std::move(rhs._elems, rhs._elems + rhs.size(), v._elems+lhs.size());
+    return v;
+}
+
+template<typename T>
+MyVector<T> operator+(const MyVector<T> &lhs, typename MyVector<T>:: value_type rhs) {
+    MyVector<T> v = lhs;
+    v.push_back(std::move(rhs));
+    return v;
+}
+
+
+template<typename T>
+MyVector<T>& MyVector<T>::operator+=(value_type&& val) {
+    push_back(std::move(val));
+    return *this;
+}
+
+template<typename T>
+MyVector<T>& MyVector<T>::operator+=(const_reference val) {
+    push_back(val);
+    return *this;
+}
+
+#endif // DEBUG
+
+
+
+
 
--- CS106L_example_code/VectorClass - Move/test.cpp
+++ CS106L_example_code/VectorClass - Move/test.cpp
@@ -1,5 +1,5 @@
 // #include "feaction.h"
-#include "doublevector.h"
+#include "MyVector.h"
 #include "intvector.h"
 #include <algorithm>
 
@@ -12,22 +12,22 @@ int main() {
     // cout << a + 10 << endl;
     // cout << 10 + a << endl;
     
-    DoubleVector v {1.12, 2.9};
+    MyVector<double> v {1.12, 2.9};
     v.push_back(6.6);
     v.insert(2, 2.4);
     v.push_back(9.9);
-    DoubleVector v2(3, 8.8);
+    MyVector<double> v2(3, 8.8);
     v2 += 4.4;
     cout << v2 << endl;
     cout << v + v2 << endl; // in the operator +, there is a constructor
-    DoubleVector v3 = std::move(v + 100);
-    cout << v3 << endl;
-    cout << v << endl;
+    MyVector<double> v3 = std::move(v + 100);
+    // cout << v3 << endl;
+    // cout << v << endl;
     v3 = std::move(v2);
     // v2.push_back(10); after applying std::move on v2, v2 cannot be used. Segmentation error
     v3.push_back(90);
-    cout << v3 << endl;
-    cout << v2 << endl;
+    // cout << v3 << endl;
+    // cout << v2 << endl;
 
     return 0;
 }
